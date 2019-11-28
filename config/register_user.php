32539d4767d30704fc7aa47f50f35cecefd08dee
GarvielL0ken <jedmikasarkis@gmail.com>
2019-11-28 12:54:27 +0200

    uses pdo preoperly now

--- config/register_user.php
+++ config/register_user.php
@@ -1,20 +1,20 @@
 <?php
-	require 'verify.php';
+	require './verify.php';
 	require './database.php';
 	require './setup.php';
 
 	$conn = connect_to_db();
 	$passwd = hash( 'whirlpool', $passwd);
-	$stmt = $conn->prepare("SELECT username FROM users WHERE username = '$username'");
-	$stmt->execute();
+	$stmt = $conn->prepare("SELECT username FROM users WHERE username = :username");
+	$stmt->execute(array("username" => $username));
 	$results = $stmt->fetchAll();
 	foreach($results as $result)
 	{
 		if ($username === $result['username']);
 			return(print("Username already in use"));
 	}
-	$sql = "INSERT INTO users (first_name, last_name, username, email_address, passwd)
-		VALUES ('$first_name', '$last_name', '$username', '$email', '$passwd')";
-	$conn->exec($sql);
-	header("Location: ../login.php");
+	$sql = 'INSERT INTO users (first_name, last_name, username, email_address, passwd)
+		VALUES (:first_name, :last_name, :username, :email_address, :passwd)';
+	$stmt = $conn->prepare($sql);
+	$stmt->execute(array("first_name" => $first_name, "last_name" => $last_name, "username" => $username, "email_address" => $email, "passwd" => $passwd));
 ?>
