2b41d50acd4abf9cb3ef3e306c72f35333beaec0
GarvielL0ken <jedmikasarkis@gmail.com>
2019-12-03 16:04:04 +0200

    added a transparent picture and a display the username on the profile page

--- site/profile.php
+++ site/profile.php
@@ -1,2 +1,15 @@
 <?php
+    $page = 'profile';
+    require_once '../config/globals.php';
+    require_once 'header.php';
 ?>
+<html>
+    <body>
+        <div class= "card" id= "frm_user_data">
+            <img src= "../resources/Salamander_0.png">
+            <?php
+                print('<h2>' . $_SESSION['logged_on_user'] . '</h2>');
+            ?>
+        </div>
+    </body>
+</html>
