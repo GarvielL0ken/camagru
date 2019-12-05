bad34f0855188ef25def8f2bda0b8ff3f920a3a8
GarvielL0ken <jedmikasarkis@gmail.com>
2019-12-05 11:42:32 +0200

    made the conditions in header.php shorter

--- site/header.php
+++ site/header.php
@@ -15,19 +15,22 @@
 			<h2>Camagru</h2>
 		</div>
 		<?php
-			if ($page == 'login' || $page == "reset_password" || $page == 'email')
+			$v = array('login' => 0, 'reset_password' => 0, 'email' => 0, 'registration' => 0, 'main' => 0, 'profile' => 0, 'upload' => 0);
+			$v[$page] = 1;
+			if ($v['login'] || $v['reset_password'] || $v['email'])
 				print(output_a('registration.php', output_input('button', 'Register', 'header_btn transparent')));
-			if ($page == 'registration' || $page == "reset_password" || $page == 'email')
+			if ($v['registration'] || $v['reset_password'] || $v['email'])
 				print(output_a('login.php', output_input('button', 'Login', 'header_btn transparent')));
-			if ($page == 'main')
+			if ($v['main'] || $v['profile'])
 			{
 				print(output_a('upload.php', output_input('button', 'Upload', 'header_btn transparent')));
 				print(output_input('button', 'Browse', 'header_btn transparent'));
-				print(output_a('profile.php', output_input('button', 'Profile', 'header_btn transparent')));
 			}
-			if ($page == 'profile' || $page == 'upload')
+			if ($v['profile'] || $v['upload'])
 				print(output_a('main.php', output_input('button', 'Main', 'header_btn transparent')));
-			if ($page == 'main' || $page == 'upload' || $page == 'profile')
+			if ($v['main'] || $v['upload'])
+				print(output_a('profile.php', output_input('button', 'Profile', 'header_btn transparent')));
+			if ($v['main'] || $v['upload'] || $v['profile'])
 				print(output_a('logout.php', (output_input('button', 'Logout', 'header_btn transparent'))));
 		?>
 	</div>
