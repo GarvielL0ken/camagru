1e0faef674553aa0086a154b1450f9740941364c
GarvielL0ken <jedmikasarkis@gmail.com>
2019-12-09 13:13:12 +0200

    now handles users changing their email addresses

--- site/verify_email.php
+++ site/verify_email.php
@@ -8,7 +8,14 @@
     $id_user = valid_hash($hash, 'new_user_hash');
     if (!$id_user)
         exit();
-    update_value('users', 'verified', 1, $id_user);
+    $new_email = is_in_db('users', 'id_user', $id_user, 'new_email')[0]['new_email'];
+    if ($new_email)
+    {
+        update_value('users', 'new_email', null, $id_user);
+        update_value('users', 'email_address', $new_email, $id_user);
+    }
+    else
+        update_value('users', 'verified', 1, $id_user);
     delete_hash($hash, 'new_user_hash');
     header("Location: ./login.php");
 ?>
